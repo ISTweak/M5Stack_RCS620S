b0291b85a81b6d6b63148ebc43be1da90e6136b3
ISTweak <osamu.utsugi@gmail.com>
2021-04-05 14:54:15 +0900

    Add example

---
+++ examples/ReadId/ReadId.ino
@@ -0,0 +1,45 @@
+#include <M5Stack.h>
+#include <RCS620S.h>
+
+#define POLLING_INTERVAL 500
+
+RCS620S rcs620s;
+
+void setup() {
+  M5.begin();
+  int ret;
+  ret = rcs620s.initDevice();
+  Serial.println("Init RCS620s ");
+  if (!ret) {
+    while(19);
+  }
+  Serial.println("Connect RCS620S");
+}
+
+void loop()
+{
+  uint8_t size, type = PICC_UNKNOWN;
+  uint8_t buf[20];
+  bool found = false;
+  
+  if (rcs620s.polling() || rcs620s.polling_typeA() || rcs620s.polling_typeB()) {
+    Serial.print("ID: ");
+    for (int i = 0; i < rcs620s.idLength; i++) {
+      Serial.print(rcs620s.idm[i], HEX);
+    }
+    type = rcs620s.piccType;
+    if (type == PICC_ISO_IEC14443_TypeA_MIFARE) {
+        Serial.print("(ISO/IEC14443 Type A MIFARE)\r\n");                
+    } else if (type == PICC_ISO_IEC14443_TypeA_MIFAREUL) {
+        Serial.print("(ISO/IEC14443 Type A MIFARE Ultralight)\r\n");                
+    } else if (type == PICC_ISO_IEC14443_TypeB) {
+        Serial.print("(ISO/IEC14443 Type B)\r\n");
+    } else if (type == PICC_FELICA) {
+        Serial.print("(FeliCa)\r\n");
+    } else {
+        Serial.print("(Unknown PICC Type)\r\n");                
+    }
+  }
+
+  delay(POLLING_INTERVAL);
+}
